b749815babb383060734f09fce59269926c42309
vinyufi <mail@webkatu.com>
2016-05-11 21:17:05 +0900

    refactoring

--- index.js
+++ index.js
@@ -253,12 +253,15 @@ var Request = function () {
   this.data = null;
   this.dispatcher = null;
   this.hash = null;
+  this.host = null;
   this.hostname = null;
   this.method = null;
+  this.origin = null;
   this.originalUrl = null;
   this.params = null;
   this.path = null;
   this.pathname = null;
+  this.port = null;
   this.protocol = null;
   this.query = null;
   this.search = null;
@@ -287,16 +290,18 @@ exports.default = Request;
 
 
 function setURL(url) {
-	this.originalUrl = url.href;
-	this.secure = url.auth;
-	this.protocol = url.protocol;
-	this.hostname = url.hostname;
-	this.port = url.port;
-	this.pathname = url.pathname;
-	this.path = url.path = url.path;
-	this.search = url.search;
 	this.hash = url.hash;
+	this.host = url.host;
+	this.hostname = url.hostname;
+	this.origin = url.origin;
+	this.originalUrl = url.href;
+	this.path = url.path = url.path;
+	this.pathname = url.pathname;
+	this.port = url.port;
+	this.protocol = url.protocol;
 	this.query = _querystring2.default.parse(url.query);
+	this.search = url.search;
+	this.secure = url.auth;
 }
 },{"./internal":10,"./namespace":13,"js-cookie":15,"querystring":3}],6:[function(require,module,exports){
 'use strict';
--- src/Request.js
+++ src/Request.js
@@ -15,12 +15,15 @@ export default class Request {
 		this.data = null;
 		this.dispatcher = null;
 		this.hash = null;
+		this.host = null;
 		this.hostname = null;
 		this.method = null;
+		this.origin = null;
 		this.originalUrl = null;
 		this.params = null;
 		this.path = null;
 		this.pathname = null;
+		this.port = null;
 		this.protocol = null;
 		this.query = null;
 		this.search = null;
@@ -44,14 +47,16 @@ export default class Request {
 
 
 function setURL(url) {
-	this.originalUrl = url.href;
-	this.secure = url.auth;
-	this.protocol = url.protocol;
-	this.hostname = url.hostname;
-	this.port = url.port;
-	this.pathname = url.pathname;
-	this.path = url.path = url.path
-	this.search = url.search;
 	this.hash = url.hash;
+	this.host = url.host;
+	this.hostname = url.hostname;
+	this.origin = url.origin;
+	this.originalUrl = url.href;
+	this.path = url.path = url.path
+	this.pathname = url.pathname;
+	this.port = url.port;
+	this.protocol = url.protocol;
 	this.query = qs.parse(url.query);
+	this.search = url.search;
+	this.secure = url.auth;
 }
